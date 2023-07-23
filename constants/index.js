810edabc5c72154db1bc251ca80758c84f2b0079
Hessam Wallet <129942357+hessamwallet@users.noreply.github.com>
2023-07-23 16:54:53 +0000

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x0eb8e597919d14986efa8e03de303a76db1e92d5";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
